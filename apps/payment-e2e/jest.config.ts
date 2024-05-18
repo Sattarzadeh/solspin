42df6b03db7e3179240cca90c31341c5a9f7a78d
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2024-05-18 15:20:00 +0100

    Initial commit

---
+++ apps/payment-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'payment-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/payment-e2e',
+};
