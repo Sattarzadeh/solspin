dc88bfff0be26924dc2d7408bf85b40137e9ba39
Mehran Sattarzadeh <54243372+MehranSattar@users.noreply.github.com>
2024-05-21 16:42:26 +0100

    Feat: Added empty methods to case data service which should be replaced once datastore architecture is finalised

--- resources/src/services/case_data_service.ts
+++ resources/src/services/case_data_service.ts
@@ -1,3 +1,21 @@
+import { Case } from "../models/case_model";
+
 export class CaseDataService {
-    
+
+    public getCase(caseName: string): Case {
+        /*
+            Access datastore (or cache) which should be indexed by caseName for efficiency and check if the caseName exists, 
+            if it does return the case object if not throw error
+        */
+        return
+    }
+
+    public getAllCases(): Array<Case> {
+        /*
+            Access datastore return all cases (pagination)
+        */
+
+        return
+    }
+
 }
