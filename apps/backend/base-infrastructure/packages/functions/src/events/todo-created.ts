05e7db8eb5177a511b495af0ff537f5200239f1e
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2024-06-07 19:31:29 +0100

    Base infra start

--- apps/backend/base-infrastructure/packages/functions/src/events/todo-created.ts
+++ apps/backend/base-infrastructure/packages/functions/src/events/todo-created.ts
@@ -1,5 +1,5 @@
 import { EventHandler } from "sst/node/event-bus";
-import { Todo } from "@vpc/core/todo";
+import { Todo } from "@base-infrastructure/core/todo";
 
 export const handler = EventHandler(Todo.Events.Created, async (evt) => {
   console.log("Todo created", evt);
