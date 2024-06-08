fbaf428052d7a61bce36e65b769110cf3982c078
armansattarzadeh <armansattarzadeh@hotmail.com>
2024-06-08 19:47:55 +0100

    added event bus and rules

--- apps/backend/base-infrastructure/sst.config.ts
+++ apps/backend/base-infrastructure/sst.config.ts
@@ -1,18 +1,21 @@
-import { SSTConfig } from "sst";
-import { VPC } from "./stacks/vpc";
-import { Event } from "./stacks/EventBus"
+import { SSTConfig } from 'sst';
+import { VPC } from './stacks/Vpc';
+import { Event } from './stacks/EventBus';
+import { TableStack } from './stacks/Table';
+import { api } from './stacks/Api';
+import { Lambdas } from './stacks/Lambda';
 
 export default {
   config(_input) {
     return {
-      name: "base-infrastructure",
-      region: "eu-west-1",
+      name: 'base-infrastructure',
+      region: 'us-west-1',
     };
   },
   stacks(app) {
     app.setDefaultFunctionProps({
-      runtime: "nodejs18.x",
-      architecture: "arm_64",
+      runtime: 'nodejs18.x',
+      architecture: 'arm_64',
       environment: {
         STAGE: app.stage,
         REGION: app.region,
@@ -20,5 +23,9 @@ export default {
     });
 
     app.stack(VPC);
-  }
+    app.stack(TableStack);
+    app.stack(Event);
+    app.stack(api);
+    app.stack(Lambdas);
+  },
 } satisfies SSTConfig;
