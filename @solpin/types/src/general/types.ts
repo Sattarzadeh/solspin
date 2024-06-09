2d1c9cdf341b24e654cd3fc92ab0e93228bbcdb1
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2024-06-09 17:54:56 +0100

    Add events and types libraries

---
+++ @solpin/types/src/general/types.ts
@@ -0,0 +1,9 @@
+// Enum value representing each microservice
+export const enum Service {
+  BETTING = "betting_service",
+  GAME = "game_service",
+  ORCHESTRATION = "orchestration_service",
+  TREASURY = "treasury_service",
+  USER = "user_service",
+  WALLET = "wallet_service",
+}
