8754ffca2907ddc4c8d958262c9b5d2b2d99a739
Mehran Sattarzadeh <54243372+MehranSattar@users.noreply.github.com>
2024-06-09 15:15:01 +0100

    Feat: removed game-engine files

--- apps/game-engine/middlewares/pageNotFound.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { PageNotFoundError } from '../errors/PageNotFoundError';
-
-export function pageNotFound(req: Request, res: Response, next: NextFunction) {
-  next(new PageNotFoundError());
-}
