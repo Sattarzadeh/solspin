b41da67cd8f27748bf9fad7d44d0e3384c1b0b96
Mehran Sattarzadeh <54243372+MehranSattar@users.noreply.github.com>
2024-06-12 14:23:49 +0100

    Feat: Removed sqs handler file

--- apps/game-engine/src/services/caseDataService.ts
+++ apps/game-engine/src/services/caseDataService.ts
@@ -1,12 +1,12 @@
-import { Case } from "../models/case_model";
-import { mockCase } from "../mock/case_pot_of_gold.mock";
+import { Case } from '../models/case_model';
+import { mockCase } from '../mock/case_pot_of_gold.mock';
 
 export const getCase = async (caseName: string): Promise<Case | null> => {
   try {
-    return mockCase["case_name"] === caseName ? mockCase : null;
+    return mockCase['case_name'] === caseName ? mockCase : null;
   } catch (error) {
-    console.error("Error reading case data:", error);
-    throw new Error("Could not read case data");
+    console.error('Error reading case data:', error);
+    throw new Error('Could not read case data');
   }
 };
 
@@ -14,7 +14,7 @@ export const getAllCases = async (): Promise<Array<Case>> => {
   try {
     return [mockCase];
   } catch (error) {
-    console.error("Error reading all cases:", error);
-    throw new Error("Could not read all cases");
+    console.error('Error reading all cases:', error);
+    throw new Error('Could not read all cases');
   }
 };
--- apps/game-engine/src/services/caseItemDeterminationService.ts
+++ apps/game-engine/src/services/caseItemDeterminationService.ts
@@ -1,12 +1,14 @@
-import { CaseItem } from "../models/case_item_model";
-import { Case } from "../models/case_model";
+import { CaseItem } from '../models/case_item_model';
+import { Case } from '../models/case_model';
 import { createHmac } from 'crypto';
 const findItem = (rollNumber: number, prefixSums: Array<number>): number => {
   let leftPointer = 0;
   let rightPointer = prefixSums.length - 1;
 
   while (leftPointer < rightPointer) {
-    const middlePointer = Math.floor(leftPointer + (rightPointer - leftPointer) / 2);
+    const middlePointer = Math.floor(
+      leftPointer + (rightPointer - leftPointer) / 2
+    );
 
     if (rollNumber > prefixSums[middlePointer]) {
       leftPointer = middlePointer + 1;
@@ -18,14 +20,16 @@ const findItem = (rollNumber: number, prefixSums: Array<number>): number => {
   return leftPointer;
 };
 
-export const determineItem = (rollNumber: number, caseModel: Case): CaseItem => {
+export const determineItem = (
+  rollNumber: number,
+  caseModel: Case
+): CaseItem => {
   const caseItemPrefixSums = caseModel.item_prefix_sums;
   const itemIndex = findItem(rollNumber, caseItemPrefixSums);
   const rewardItem = caseModel.items[itemIndex];
 
   return rewardItem;
 };
-
 
 const MAX_HEX_SEGMENTS = 6;
 const HEX_SEGMENT_SIZE = 2;
@@ -34,34 +38,49 @@ const HASH_TYPE = 'sha256';
 const TICKET_QUANTITY = 99999;
 
 function calculateDecimalValue(preResult: string): number {
-    let decimalValue = 0;
-    for (let i = 0; i < MAX_HEX_SEGMENTS; i++) {
-        const hexValue = preResult.substr(HEX_SEGMENT_SIZE * i, HEX_SEGMENT_SIZE);
-        decimalValue += parseInt(hexValue, 16) / Math.pow(BASE_FOR_HEX_CONVERSION, i + 1);
-    }
-    return decimalValue;
+  let decimalValue = 0;
+  for (let i = 0; i < MAX_HEX_SEGMENTS; i++) {
+    const hexValue = preResult.substr(HEX_SEGMENT_SIZE * i, HEX_SEGMENT_SIZE);
+    decimalValue +=
+      parseInt(hexValue, 16) / Math.pow(BASE_FOR_HEX_CONVERSION, i + 1);
+  }
+  return decimalValue;
 }
 
-function getProvablyFairResult(init: string, serverSeed: string, qty: number): { preResult: string, result: number } {
-    const preResult = createHmac(HASH_TYPE, serverSeed).update(init).digest('hex');
-    const decimalValue = calculateDecimalValue(preResult);
-    const result = Math.floor(decimalValue * qty) + 1;
-    return {
-        preResult: preResult,
-        result: result,
-    };
+function getProvablyFairResult(
+  init: string,
+  serverSeed: string,
+  qty: number
+): { preResult: string; result: number } {
+  const preResult = createHmac(HASH_TYPE, serverSeed)
+    .update(init)
+    .digest('hex');
+  const decimalValue = calculateDecimalValue(preResult);
+  const result = Math.floor(decimalValue * qty) + 1;
+  return {
+    preResult: preResult,
+    result: result,
+  };
 }
 
-export const generateRollValue = (serverSeed: string, clientSeed: string, nonce: number): number => {
-    if (!serverSeed || !clientSeed || nonce < 0) {
-        throw new Error('Invalid inputs');
-    }
+export const generateRollValue = (
+  serverSeed: string,
+  clientSeed: string,
+  nonce: number
+): number => {
+  if (!serverSeed || !clientSeed || nonce < 0) {
+    throw new Error('Invalid inputs');
+  }
 
-    const sanitizedServerSeed = serverSeed.replace(/\r|\n/g, '');
-    const sanitizedClientSeed = clientSeed.replace(/\r|\n/g, '');
-    const nrand = sanitizedClientSeed.length;
-    const stringToHash = `${sanitizedClientSeed}-${nrand}-${nonce}`;
+  const sanitizedServerSeed = serverSeed.replace(/\r|\n/g, '');
+  const sanitizedClientSeed = clientSeed.replace(/\r|\n/g, '');
+  const nrand = sanitizedClientSeed.length;
+  const stringToHash = `${sanitizedClientSeed}-${nrand}-${nonce}`;
 
-    const { result } = getProvablyFairResult(stringToHash, sanitizedServerSeed, TICKET_QUANTITY);
-    return result;
-}
+  const { result } = getProvablyFairResult(
+    stringToHash,
+    sanitizedServerSeed,
+    TICKET_QUANTITY
+  );
+  return result;
+};
