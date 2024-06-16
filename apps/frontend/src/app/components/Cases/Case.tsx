af4de21f3dab0eddde2dd0582a0a33f1f3169e66
armansattarzadeh <armansattarzadeh@hotmail.com>
2024-06-16 18:18:48 +0100

    removed dollar character

--- apps/frontend/src/app/components/Cases/Case.tsx
+++ apps/frontend/src/app/components/Cases/Case.tsx
@@ -32,7 +32,7 @@ export const Case: React.FC<CaseProps> = ({ name, price, rarity, tag, image }) =
         <span className="text-white font-bold text-sm">{name}</span>
         <div className="flex items-center justify-between gap-1">
           <img src="/icons/dollar.svg" alt="Dollar" className="h-6 w-6" />
-          <span className="gradient-text">{"$" + price}</span>
+          <span className="gradient-text">{price}</span>
         </div>
       </div>
     </div>
