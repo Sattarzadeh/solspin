8c72dc7c7cc88bfefc13ad7b66a16841cf9f9f37
armansattarzadeh <armansattarzadeh@hotmail.com>
2024-06-24 22:27:06 +0100

    feat: consume events from GameResultRule

--- apps/backend/services/betting/src/service/events/handler/create-bet.ts
+++ apps/backend/services/betting/src/service/events/handler/create-bet.ts
@@ -1,44 +1,32 @@
 import { ZodError } from "zod";
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { EventBridgeEvent } from "aws-lambda";
 import { Service } from "@solspin/types";
 import { Betting, BetTransaction, publishEvent } from "@solspin/events";
 import { recordBet } from "../../../data-access/record-bet";
 import { errorResponse, successResponse } from "@solspin/gateway-responses";
 import { getLogger } from "@solspin/logger";
 import { EVENT_BUS_ARN } from "../../../foundation/runtime";
+import { CreateBetEvent, CreateBetRequestSchema } from "../schemas/schema";
 
 const logger = getLogger("create-bet-handler");
 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+export const handler = async (event: EventBridgeEvent<"CreateBetEvent", CreateBetEvent>) => {
   logger.info("Received create bet request", { event });
 
   try {
-    const parsedBody = JSON.parse(event.body || "{}");
-
-    let createBetRequest;
-    try {
-      createBetRequest = Betting.CreateBetRequestSchema.parse(parsedBody);
-    } catch (error) {
-      if (error instanceof ZodError) {
-        logger.error("Validation error creating bet", { error });
-        return errorResponse(error as Error, 400);
-      }
-      throw error;
-    }
+    const eventDetails = CreateBetRequestSchema.parse(event.detail);
+    const { userId, gameType, amountBet, outcome, outcomeAmount } = eventDetails.payload;
 
     // TODO - Check user exists
-
     // TODO - Check game exists
 
-    const { userId, gameId, amountBet, outcome, outcomeAmount } = createBetRequest;
-
-    const createdBet = await recordBet(userId, gameId, amountBet, outcome, outcomeAmount);
+    const createdBet = await recordBet(userId, gameType, amountBet, outcome, outcomeAmount);
 
     const response = Betting.CreateBetResponseSchema.parse(createdBet);
 
-    console.log("eventbusname", EVENT_BUS_ARN);
+    logger.info("EventBus ARN", { EVENT_BUS_ARN });
 
-    publishEvent(
+    await publishEvent(
       BetTransaction.event,
       {
         userId,
@@ -52,12 +40,11 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     return successResponse(response);
   } catch (error) {
     if (error instanceof ZodError) {
-      logger.error("Validation error creating bet", { error });
-      return errorResponse(error as Error, 400);
+      logger.error("Validation error creating bet", { error: error.errors });
+      return errorResponse(error, 400);
     }
 
     logger.error("Error creating bet", { error });
-
     return errorResponse(error as Error);
   }
 };
