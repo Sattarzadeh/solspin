37ddbfb1ee14811901666453d98d2bf92f9db2cf
Mehran Sattarzadeh <54243372+MehranSattar@users.noreply.github.com>
2024-07-01 20:46:56 +0100

    Changed to authorization header

--- apps/backend/services/websocket/src/helpers/callAuthorizer.ts
+++ apps/backend/services/websocket/src/helpers/callAuthorizer.ts
@@ -3,11 +3,13 @@ import * as AWS from "aws-sdk";
 const lambda = new AWS.Lambda();
 
 export const callAuthorizer = async (token: string) => {
+  const bearerToken = `Bearer ${token}`;
+
   const params = {
     FunctionName: process.env.AUTHORIZER_FUNCTION_NAME,
     Payload: JSON.stringify({
-      queryStringParameters: {
-        token,
+      headers: {
+        Authorization: bearerToken,
       },
     }),
   };
